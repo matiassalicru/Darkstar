2e1e7bfd5e0c67165b9a9a4ff3ae598ddece28ac
Matias Salicru <mattiassalicru@gmail.com>
2021-12-11 16:35:08 -0300

    Improving Cart component

--- src/components/common/Cart.js
+++ src/components/common/Cart.js
@@ -1,34 +1,31 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeCart } from '../../actions/ui';
 import { CartItem } from './CartItem';
-
-// import emptyBox from "../../Assets/emptyBox.svg";
-// import { Link } from "react-router-dom";
 
 export const Cart = () => {
 	const dispatch = useDispatch();
 	const items = useSelector((state) => state.cart.items);
 	const total = useSelector((state) => state.cart.total);
 
-	const setCartClosed = () => {
+	const onCloseCart = () => {
 		dispatch(closeCart());
 	};
 
 	return (
 		<aside className="cart__main animate__animated animate__fadeInRight animate__faster">
-			<div className={`cart__exit`} onClick={setCartClosed}>
+			<div className="cart__exit" onClick={onCloseCart}>
 				<i className="fas fa-times"></i>
 			</div>
 
 			<div className="cart__items">
-				{items.length >= 1 ? (
-					<Fragment>
+				{!!items.length ? (
+					<>
 						<p className="cart__total">Tú pedido: ${total} ARS</p>
 						{items.map((item) => (
 							<CartItem key={item.id} item={item} />
 						))}
-					</Fragment>
+					</>
 				) : (
 					<section className="cart__emptyCart">
 						<h3>Aun no tienes accesorios en tu carrito</h3>
